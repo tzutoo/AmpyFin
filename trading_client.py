37e05495aab37074d6ace7debc13114074241cb9
Yeon Lee <yeonholee50@gmail.com>
2024-12-19 10:02:20 -0500

    added sell loop escape clause to help buy incentive more

--- trading_client.py
+++ trading_client.py
@@ -170,7 +170,8 @@ def main():
                         decision, quantity = simulate_strategy(strategy, ticker, current_price, historical_data,
                                                       buying_power, portfolio_qty, portfolio_value)
                         weight = strategy_to_coefficient[strategy.__name__]
-                        
+                        if decision == 'hold' and quantity == 1:
+                            decision = 'sell'
                         decisions_and_quantities.append((decision, quantity, weight))
                     decision, quantity, buy_weight, sell_weight, hold_weight = weighted_majority_decision_and_median_quantity(decisions_and_quantities)
                     print(f"Ticker: {ticker}, Decision: {decision}, Quantity: {quantity}, Weights: Buy: {buy_weight}, Sell: {sell_weight}, Hold: {hold_weight}")
