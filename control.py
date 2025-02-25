48a5fe0f5d42c54c373448684443e0b0698e2bf6
Yeon Lee <yeonholee50@gmail.com>
2025-02-25 13:22:27 -0500

    added default indicators

--- control.py
+++ control.py
@@ -38,7 +38,7 @@ There will be an option to:
 'push' means pushing your trained bot to the database. This is only available for the ranking client.
 The default for mode is live to protect against accidental training
 """
-mode = 'test'
+mode = 'train'
 
 """
 training parameters - run purely on ranking_client.py
@@ -49,8 +49,8 @@ please keep in mind training takes quite a long time. Our team trained it on a 1
 so please understand the time it takes to train.
 
 """
-period_start = "2003-07-20"
-period_end = "2003-08-05"
+period_start = "2001-01-01"
+period_end = "2009-12-31"
 train_tickers = []
 
 """
